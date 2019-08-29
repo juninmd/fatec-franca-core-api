914b6af2d67cf196862fbf0156f70afb731b5a95
Antonio Carlos <jr_acn@yahoo.com.br>
2019-08-29 20:41:44 -0300

    Adicionando arquivo jest.config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  bail: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: null,
+  testRegex: "index.test.ts",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/__tests__/", "<rootDir>/node_modules/"]
+};
